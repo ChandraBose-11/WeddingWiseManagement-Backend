14d885f89278c87e341baac42cd6d7c5eca70eaa
ChandraBose-11 <chandrubose46@gmail.com>
2024-11-07 11:46:27 +0530

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./Database/config.js";
+import authRouter from "./Routers/authRouter.js";
+import userRouter from "./Routers/userRouter.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  origin: "*",
+  credentials: true,  
+}));
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to my API" });
+});
+
+// API routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+// Board Routes
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on`);
+});
